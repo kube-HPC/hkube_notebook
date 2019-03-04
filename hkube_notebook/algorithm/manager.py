4cb38275018765cbaa16a65bdaba7dd4b54b1ec6
Amir Yiron <amiryi@rnd-hub.com>
2019-03-04 14:49:48 +0200

    sync/async apply; apply by github; ds example notebook

--- hkube_notebook/algorithm/manager.py
+++ hkube_notebook/algorithm/manager.py
@@ -3,11 +3,14 @@ import json
 import inspect
 import tarfile
 import os
+import time
 import platform
 import getpass
 import git
 from git import RemoteProgress
 import shutil
+import pathlib
+import subprocess
 from ..api_utils import report_request_error, is_success, JSON_HEADERS, FORM_URLENCODED_HEADERS
 
 class CustomProgress(RemoteProgress):
@@ -15,17 +18,35 @@ class CustomProgress(RemoteProgress):
         if message:
             print(message, op_code, cur_count, max_count)
 
+class TextSpinner(object):
+    SPINNER_STATES = ('-', '/', '|', '\\')
+    
+    def __init__(self):
+        self._i = 0
+    
+    def get_next(self, text):
+        result = f'{TextSpinner.SPINNER_STATES[self._i]} {text}'
+        self._i += 1
+        if self._i >= len(TextSpinner.SPINNER_STATES):
+            self._i = 0
+        return result
+
 class AlgorithmBuilder(object):
     """ Builds, views and deletes algorithms in hkube """
 
+    POLL_INTERVAL_SEC = 1
+    
     def __init__(self, api_server_base_url):
         self._base_url = api_server_base_url
 
     def _get_store_url(self):
-        return '{base}/store/algorithms'.format(base=self._base_url)
+        return f'{self._base_url}/store/algorithms'
 
     def _get_apply_url(self):
-        return '{base}/apply/algorithms'.format(base=self._base_url)
+        return f'{self._base_url}/store/algorithms/apply'
+
+    def _get_build_status_url(self, build_id):
+        return f'{self._base_url}/builds/status/{build_id}'
 
     def get_all(self, only_names=False):
         """ Get all algorithms """
@@ -73,26 +94,79 @@ class AlgorithmBuilder(object):
         print("OK: algorithm {name} was deleted successfully".format(name=alg_name))
         return True
     
-    def apply(self, compressed_alg_file, config):
-        """ Request to build/rebuild an algorithm image """
-        files = {'zip': open(compressed_alg_file,'rb')}
+    def get_build_state(self, build_id, verbose=True):
+        """ return algorithm build state (including status and all algorithm properties) """
+        status_url = self._get_build_status_url(build_id)
+        response = requests.get(status_url, verify=False)
+        if not is_success(response):
+            report_request_error(response, 'get algorithm build status')
+            return None
+        json_data = json.loads(response.text)
+        alg = json_data['algorithm']
+        name = alg['name']
+        status = json_data['status']
+        if verbose:
+            print(f'algorithm {name} buildId {build_id} status: {status}')
+        return json_data
+
+    def apply_async(self, compressed_alg_file, config):
+        """ 
+        Async request to build/rebuild an algorithm image
+        :param compressed_alg_file algorithm tar.gz filename
+        :param config algorithm configuration
+        :return algorithm build state - if not includes buildId then build is completed
+        """
+        name = config['name']
+        files = {'file': open(compressed_alg_file,'rb')}
         json_config = json.dumps(config)
         values = { 'payload': json_config }
         response = requests.post(self._get_apply_url(), files=files, data=values, verify=False)
         if not is_success(response):
-            report_request_error(response, 'apply algorithm "{name}"'.format(name=config['name']))
-            return False
+            report_request_error(response, f'build algorithm "{name}"')
+            return None
+        state = json.loads(response.text)
         
-        print("OK: algorithm {name} was applied successfully".format(name=config['name']))
-        return True
+        return state
 
-    def create_config(self, alg_name, entryfile, cpu=1, mem='512Mi', minHotWorkers=0, alg_env=None, worker_env=None, options=None):
+    def apply(self, compressed_alg_file, config):
+        """ 
+        Blocking request to build/rebuild an algorithm image
+        :param compressed_alg_file algorithm tar.gz filename
+        :param config algorithm configuration
+        :return algorithm state (including status and all algorithm properties)
+        """
+        name = config['name']
+        state = self.apply_async(compressed_alg_file, config)
+        if state is None:
+            return None
+        if "buildId" in state.keys():
+            current_milli_time = lambda: int(round(time.time() * 1000))
+            start_time = current_milli_time()
+            build_id = state['buildId']
+            print(f'algorithm buildId: {build_id}')
+            # poll build status
+            status = ''
+            spinner = TextSpinner()
+            while not (status=='completed' or status=='failed'):
+                state = self.get_build_state(build_id, verbose=False)
+                if state is None:
+                    return None
+                status = state['status']
+                print(spinner.get_next(f'...building algorithm {name} - status: {status}               '), end='\r')
+                time.sleep(AlgorithmBuilder.POLL_INTERVAL_SEC)
+                
+        build_time_sec = (current_milli_time() - start_time)/1000
+        if status == 'completed':
+            print(f"OK: algorithm {name} was built successfully in {build_time_sec} seconds                ")
+            return state
+        print(f'Algorithm {name} build failed after {build_time_sec} seconds')
+        return state
+
+    def create_config(self, alg_name, entryfile, cpu=1, mem='512Mi', minHotWorkers=0, version=None, alg_env=None, worker_env=None, options=None):
         config = {
             "name": alg_name,
             "env": "python",
-            "code": {
-                "entryPoint": entryfile
-            },
+            "entryPoint": entryfile,
             "algorithmImage": "hkube/{}".format(alg_name),
             "cpu": cpu,
             "mem": mem,
@@ -109,17 +183,20 @@ class AlgorithmBuilder(object):
             config['algorithmEnv'] = alg_env
         if type(options) is dict:
             config['options'] = options
+        if version is not None:
+            config['version'] = version
         return config
 
 
-    def create_algfile_by_functions(self, init_func, start_func, stop_func, exit_func):
+    def create_algfile_by_functions(self, start_func, init_func=None, stop_func=None, exit_func=None):
         """ 
-        Create algorithm code from given functions inplementations then compress to tar.gz
+        Create algorithm code from given functions inplementations then compress to tar.gz.
+        NOTE: given funcions must include the whole code (imports, internal functions as nested, etc.)
 
-        :param init_func algorithm 'init' function
         :param start_func algorithm 'start' function
-        :param stop_func algorithm 'stop' function
-        :param exit_func algorithm 'exit' function
+        :param init_func algorithm 'init' function (optional)
+        :param stop_func algorithm 'stop' function (optional)
+        :param exit_func algorithm 'exit' function (optional)
         :return entry_filename, compressed_filename
         """
         # create alg file code
@@ -133,42 +210,59 @@ class AlgorithmBuilder(object):
         for func_info in func_list:
             try:
                 func = func_info[1]
-                func_name = func_info[0]
-                func_code = inspect.getsource(func)
-                def_func = 'def {func_name}'.format(func_name=func.__name__)
-                func_code = func_code.replace(def_func, 'def ' + func_name, 1)
-                alg_code += (func_code + '\n')
+                if func is not None:
+                    func_name = func_info[0]
+                    func_code = inspect.getsource(func)
+                    def_func = 'def {func_name}'.format(func_name=func.__name__)
+                    func_code = func_code.replace(def_func, 'def ' + func_name, 1)
+                    alg_code += (func_code + '\n')
             except Exception as error:
                 print('failure: {}'.format(error))
                 return None
         
         # write to file
+        dirname = 'temp'
+        pathlib.Path(dirname).mkdir(exist_ok=True)
         filename = 'alg.py'
-        fd = open(filename, "w")
+        fd = open(f'{dirname}/{filename}', "w")
         fd.write(alg_code)
         fd.close()
-
+        # run_state = subprocess.run(f'pipreqs --force {dirname}', shell=True)  
+        run_state = subprocess.run(['pipreqs', '--force',  dirname])  
+        if not run_state.returncode == 0:
+            print(f'ERROR: failed to create requirements.txt file (error: {run_state.stderr}, stderr: {run_state.stdout})')
+            return filename, None
+        # NOTE: it works only when 'pipreqs' is installed in target python env.
+        
         # create tar.gz file
-        tarfilename = '{cwd}/alg.tar.gz'.format(cwd=os.getcwd())
-        with tarfile.open(tarfilename, mode='w:gz') as archive:
-            archive.add(filename)
+        tarfilename = self.create_algfile_by_folder(folder_path=dirname, files_list=[filename, 'requirements.txt'])
 
         return filename, tarfilename
 
 
-    def create_algfile_by_folder(self, folder_path):
-        """ Compress given python algorithm folder content to tar.gz """
+    def create_algfile_by_folder(self, folder_path, files_list=None):
+        """ 
+        Compress given python algorithm folder content (or given files) to tar.gz
+
+        :param folder_path path to source folder of which we compress
+        :param files_list if None insert recursively all folder contents, else insert given files.
+        :return tarfilename
+        """
         # create tar.gz file recursively from all folder contents
         tarfilename = '{cwd}/alg.tar.gz'.format(cwd=os.getcwd())
-        # folderpath_aslist = folder_path.split('/')
-        # just_folder = folderpath_aslist.pop()
-        # path_tofolder = '/'.join(folderpath_aslist)
         cwd = os.getcwd()
-        folder_content_list = os.listdir(folder_path)
-        os.chdir(folder_path)
-        with tarfile.open(tarfilename, mode='w:gz') as archive:
-            for file in folder_content_list:
-                archive.add(file, recursive=True)
+        try:
+            if files_list is None:
+                files_list = os.listdir(folder_path)
+            os.chdir(folder_path)
+            with tarfile.open(tarfilename, mode='w:gz') as archive:
+                for file in files_list:
+                    archive.add(file, recursive=True)
+        except Exception as error:
+            print(f'ERROR: failed to create tar file {tarfilename}: {error.__str__()}')
+            os.chdir(cwd)
+            return None
+        
         os.chdir(cwd)
         return tarfilename
 
--- hkube_notebook/test/test_alg.py
+++ hkube_notebook/test/test_alg.py
@@ -14,6 +14,37 @@ api_server = 'http://localhost:3000/api/v1'
 alg_mgr = AlgorithmBuilder(api_server)
 alg_list = alg_mgr.get_all()
 
+# a self contained function
+def my_start(args):
+    import numpy as np
+    import pandas as pd
+    import time
+    n = 1000
+    df = pd.DataFrame({'x': np.random.randint(0, 5, size=n), 'y': np.random.normal(size=n)})
+    print('COLUMNS', df.columns)
+
+    input = args["input"]
+    print(f'algorithm: start, input: {input}')
+    print('working...')
+    time.sleep(5)
+    array = input[0]
+    order = input[1]
+    if order == 'asc':
+        reverse = False
+    elif order == 'desc':
+        reverse = True
+    else:
+        raise Exception('order not supported')
+
+    list.sort(array, reverse=reverse)
+    return array
+
+# create compressed alg by given implementation functions
+entryfile, tarfilename = alg_mgr.create_algfile_by_functions(my_start)
+config = alg_mgr.create_config('sort-alg', entryfile)
+alg_mgr.apply(compressed_alg_file=tarfilename, config=config)
+
+
 # create compressed alg by given implementation functions
 # entryfile, tarfilename = alg_mgr.create_algfile_by_functions(bar, stam, bar, stam)
 # config = alg_mgr.create_config('amir-alg', entryfile)
@@ -24,32 +55,3 @@ folder = 'hkube_notebook/test/test_algorithm'
 tarfilename = alg_mgr.create_algfile_by_folder(folder)
 config = alg_mgr.create_config('test-alg', 'main.py')
 alg_mgr.apply(compressed_alg_file=tarfilename, config=config)
-
-
-# alg_file = '/home/amiryi/dev/hkube/Algorithm/python-sort-alg/algorithm.py.tar.gz'
-# alg_config = {
-#        "name": "sort-alg",
-#        "env": "python",
-#        "code": {
-#            "entryPoint": entryfile
-#        },
-#        "algorithmImage": "hkube/sort-alg",
-#        "cpu": 0.1,
-#        "mem": "512Mi",
-#        "algorithmEnv": {
-#            "ENV1": "bla-bla",
-#            "ENV2": "bla-bla"
-#        },
-#        "workerEnv": {
-#            "ENV1": "bla-bla",
-#            "ENV2": "bla-bla"
-#        },
-#        "minHotWorkers": 0,
-#        "userInfo": {
-#            "platform": "linux",
-#            "hostname": "ubuntu-amiryi-ww",
-#            "username": "amiryi"
-#        }
-#    }
-# AlgorithmManager.apply(api_server, alg_file=tarfilename, config=alg_config)
-
